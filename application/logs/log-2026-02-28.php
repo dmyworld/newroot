7abd214afd751965d0c221520c457dc3f481f649
Dashan Korala Hewage <dmyworld.com@gmail.com>
2026-02-28 14:06:43 +0530

    Create log-2026-02-28.php

---
+++ application/logs/log-2026-02-28.php
@@ -0,0 +1,37 @@
+<?php  defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2026-02-28 08:34:31 --> Config Class Initialized
+INFO - 2026-02-28 08:34:31 --> Hooks Class Initialized
+DEBUG - 2026-02-28 08:34:31 --> UTF-8 Support Enabled
+INFO - 2026-02-28 08:34:31 --> Utf8 Class Initialized
+INFO - 2026-02-28 08:34:31 --> URI Class Initialized
+INFO - 2026-02-28 08:34:31 --> Router Class Initialized
+INFO - 2026-02-28 08:34:31 --> Output Class Initialized
+INFO - 2026-02-28 08:34:31 --> Security Class Initialized
+DEBUG - 2026-02-28 08:34:31 --> Global POST, GET and COOKIE data sanitized
+INFO - 2026-02-28 08:34:31 --> Input Class Initialized
+INFO - 2026-02-28 08:34:31 --> Language Class Initialized
+INFO - 2026-02-28 08:34:31 --> Loader Class Initialized
+INFO - 2026-02-28 08:34:31 --> Helper loaded: url_helper
+INFO - 2026-02-28 08:34:31 --> Helper loaded: siteconfig_helper
+INFO - 2026-02-28 08:34:31 --> Helper loaded: form_helper
+INFO - 2026-02-28 08:34:31 --> Helper loaded: language_helper
+INFO - 2026-02-28 08:34:31 --> Language file loaded: language/english/english_lang.php
+INFO - 2026-02-28 08:34:31 --> Database Driver Class Initialized
+INFO - 2026-02-28 08:34:31 --> Controller Class Initialized
+INFO - 2026-02-28 14:04:32 --> Database Driver Class Initialized
+INFO - 2026-02-28 14:04:32 --> Controller Class Initialized
+INFO - 2026-02-28 14:04:32 --> Session: Class initialized using 'database' driver.
+INFO - 2026-02-28 14:04:32 --> Language file loaded: language/english/aauth_lang.php
+DEBUG - 2026-02-28 14:04:32 --> Config file loaded: C:\Users\user\Documents\GitHub\www\newroot\application\config/aauth.php
+INFO - 2026-02-28 14:04:32 --> Database Driver Class Initialized
+INFO - 2026-02-28 14:04:32 --> Database Forge Class Initialized
+INFO - 2026-02-28 14:04:32 --> Model "Marketplace_model" initialized
+INFO - 2026-02-28 14:04:32 --> Model "Wood_types_model" initialized
+INFO - 2026-02-28 14:04:32 --> Model "Worker_model" initialized
+INFO - 2026-02-28 14:04:32 --> Model "Audit_model" initialized
+INFO - 2026-02-28 14:04:32 --> File loaded: C:\Users\user\Documents\GitHub\www\newroot\application\views\shop/header.php
+INFO - 2026-02-28 14:04:32 --> File loaded: C:\Users\user\Documents\GitHub\www\newroot\application\views\shop/footer.php
+INFO - 2026-02-28 14:04:32 --> File loaded: C:\Users\user\Documents\GitHub\www\newroot\application\views\shop/index.php
+INFO - 2026-02-28 14:04:32 --> Final output sent to browser
+DEBUG - 2026-02-28 14:04:32 --> Total execution time: 0.2059
